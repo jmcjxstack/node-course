1643303a16adb1a31fc0eceb78912d15fa980253
Jose Miguel Carvajal Jimenez <josemiguel_carvajal@epam.com>
2023-11-22 18:10:50 -0500

    fixed the issue and added output to file

--- task2/activityMonitor.js
+++ task2/activityMonitor.js
@@ -1,34 +1,65 @@
 const { exec } = require("child_process");
-const os = require("os");
+const fs = require("fs");
 
-function clearLine() {
-    process.stdout.moveCursor(0, -1);
-    process.stdout.clearLine(1);
-}
-
-function runCommand() {
-    clearLine();
-    const isWindows = os.platform() === "win32";
-
-    const command = isWindows
-        ? `powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + ' ' + $_.CPU + ' ' + $_.WorkingSet }"`
-        : `ps -A -o %cpu,%mem,comm | sort -nr | head -n 1`;
-
+function runCommand(command) {
     exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing command: ${error.message}`);
             return;
         }
-
         if (stderr) {
-            console.error(`Command stderr: ${stderr}`);
+            console.error(`Command execution resulted in an error: ${stderr}`);
             return;
         }
 
-        process.stdout.write(`Command output: ${stdout}\r\r`);
+        const currentTime = new Date().getTime();
+        const logEntry = `${currentTime} : ${stdout.trim()}\n`;
+
+        // Clear the current line and print the new result
+        process.stdout.write(`\r${stdout.trim()}`);
+
+        // Append the log entry to the file
+        fs.appendFile("activityMonitor.log", logEntry, (err) => {
+            if (err) {
+                console.error(
+                    `Error appending to the log file: ${err.message}`
+                );
+            }
+        });
     });
 }
 
-runCommand();
+function main() {
+    function runSystemCommand() {
+        const os = process.platform;
+        let command = "";
 
-setInterval(runCommand, 2000);
+        if (os === "win32") {
+            command =
+                "powershell \"Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + '  ' + $_.CPU + '  '  + $_.WorkingSet }\"";
+        } else {
+            command = "ps -A -o %cpu,%mem,comm | sort -nr | head -n 1";
+        }
+
+        runCommand(command);
+    }
+
+    // Run the command every second
+    setInterval(runSystemCommand, 1000);
+
+    // Append to log file every minute
+    setInterval(() => {
+        const currentTime = new Date().getTime();
+        const logEntry = `${currentTime} : Log entry from interval\n`;
+
+        fs.appendFile("activityMonitor.log", logEntry, (err) => {
+            if (err) {
+                console.error(
+                    `Error appending to the log file: ${err.message}`
+                );
+            }
+        });
+    }, 60000); // 60,000 milliseconds = 1 minute
+}
+
+main();
