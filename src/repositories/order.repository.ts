ed676a07b14c566bd5e048e4f502426b68a6a64c
Jose Miguel Carvajal Jimenez <josemiguel_carvajal@epam.com>
2024-01-16 09:44:53 -0500

    finished task 8

--- src/repositories/order.repository.ts
+++ src/repositories/order.repository.ts
@@ -4,41 +4,41 @@ import path from "path";
 import { OrderEntity } from "../schemas/order.entity";
 
 export class OrderRepository {
-    private ordersFilePath: string = path.join(
-        __dirname,
-        "../data/orders.json"
-    );
+	private ordersFilePath: string = path.join(
+		__dirname,
+		"../data/orders.json"
+	);
 
-    // Method to get list of orders
-    async getOrders(): Promise<OrderEntity[]> {
-        try {
-            // Reads the contents of the file
-            const content: string = await fs.readFile(
-                this.ordersFilePath,
-                "utf-8"
-            );
+	// Method to get list of orders
+	async getOrders(): Promise<OrderEntity[]> {
+		try {
+			// Reads the contents of the file
+			const content: string = await fs.readFile(
+				this.ordersFilePath,
+				"utf-8"
+			);
 
-            // Returns the contents of the file parsed as an object
-            return JSON.parse(content);
-        } catch (error) {
-            // Error handling
-            console.log(`Error getting list of orders: ${error}`);
-            return [];
-        }
-    }
+			// Returns the contents of the file parsed as an object
+			return JSON.parse(content);
+		} catch (error) {
+			// Error handling
+			console.log(`Error getting list of orders: ${error}`);
+			return [];
+		}
+	}
 
-    // Method to save orders
-    async saveOrders(orders: Record<string, any>[]): Promise<void> {
-        try {
-            // Stringifies array of orders and saves it to file
-            await fs.writeFile(
-                this.ordersFilePath,
-                JSON.stringify(orders, null, 2),
-                "utf-8"
-            );
-        } catch (error) {
-            // Error handling
-            console.log(`Error writing to file: ${error}`);
-        }
-    }
+	// Method to save orders
+	async saveOrders(orders: OrderEntity[]): Promise<void> {
+		try {
+			// Stringifies array of orders and saves it to file
+			await fs.writeFile(
+				this.ordersFilePath,
+				JSON.stringify(orders, null, 2),
+				"utf-8"
+			);
+		} catch (error) {
+			// Error handling
+			console.log(`Error writing to file: ${error}`);
+		}
+	}
 }
