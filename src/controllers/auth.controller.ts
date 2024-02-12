73a1f4acc0d3320a3850a7987fd8b0c50def915e
Jose Miguel Carvajal Jimenez <josemiguel_carvajal@epam.com>
2024-02-12 11:47:03 -0500

    format files

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,60 +4,60 @@ import { AuthService } from "../services/auth.service";
 import { UserRepository } from "../repositories/user.repository";
 
 export async function registerUser(req: Request, res: Response) {
-	try {
-		// Destructuring object from request body
-		const { email, password, role } = req.body;
+    try {
+        // Destructuring object from request body
+        const { email, password, role } = req.body;
 
-		// New instance of UserRepository
-		const userRepository = new UserRepository();
+        // New instance of UserRepository
+        const userRepository = new UserRepository();
 
-		// New instance of AuthService with userRepository as argument for the constructor
-		const authService = new AuthService(userRepository);
+        // New instance of AuthService with userRepository as argument for the constructor
+        const authService = new AuthService(userRepository);
 
-		// Wait for result of service to register user
-		const result: Record<string, any> = await authService.registerUser(
-			email,
-			password,
-			role
-		);
+        // Wait for result of service to register user
+        const result: Record<string, any> = await authService.registerUser(
+            email,
+            password,
+            role
+        );
 
-		// Return both status code and response
-		return res.status(result.code).json(result.response);
-	} catch (error) {
-		// Error handling
-		console.error(error);
-		return res.status(500).json({
-			data: null,
-			error: { message: "Internal Server error" },
-		});
-	}
+        // Return both status code and response
+        return res.status(result.code).json(result.response);
+    } catch (error) {
+        // Error handling
+        console.error(error);
+        return res.status(500).json({
+            data: null,
+            error: { message: "Internal Server error" },
+        });
+    }
 }
 
 export async function loginUser(req: Request, res: Response) {
-	try {
-		// Destructuring object from request body
-		const { email, password } = req.body;
+    try {
+        // Destructuring object from request body
+        const { email, password } = req.body;
 
-		// New instance of UserRepository
-		const userRepository = new UserRepository();
+        // New instance of UserRepository
+        const userRepository = new UserRepository();
 
-		// New instance of AuthService with userRepository as argument for the constructor
-		const authService = new AuthService(userRepository);
+        // New instance of AuthService with userRepository as argument for the constructor
+        const authService = new AuthService(userRepository);
 
-		// Wait for result of service to login user
-		const result: Record<string, any> = await authService.loginUser(
-			email,
-			password
-		);
+        // Wait for result of service to login user
+        const result: Record<string, any> = await authService.loginUser(
+            email,
+            password
+        );
 
-		// Return both status code and response
-		return res.status(result.code).json(result.response);
-	} catch (error) {
-		// Error handling
-		console.error(error);
-		return res.status(500).json({
-			data: null,
-			error: { message: "Internal Server error" },
-		});
-	}
+        // Return both status code and response
+        return res.status(result.code).json(result.response);
+    } catch (error) {
+        // Error handling
+        console.error(error);
+        return res.status(500).json({
+            data: null,
+            error: { message: "Internal Server error" },
+        });
+    }
 }
--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -6,120 +6,120 @@ import { UserRepository } from "../repositories/user.repository";
 import { ProductsRepository } from "../repositories/products.repository";
 
 export async function getCart(req: Request, res: Response) {
-	try {
-		// Get request headers
-		const headers: Record<string, any> = req.headers;
+    try {
+        // Get request headers
+        const headers: Record<string, any> = req.headers;
 
-		// New instance of CartRepository
-		const cartRepository = new CartRepository();
+        // New instance of CartRepository
+        const cartRepository = new CartRepository();
 
-		// New instance of UserRepository
-		const userRepository = new UserRepository();
+        // New instance of UserRepository
+        const userRepository = new UserRepository();
 
-		// New instance of ProductsRepository
-		const productsRepository = new ProductsRepository();
+        // New instance of ProductsRepository
+        const productsRepository = new ProductsRepository();
 
-		// New instance of CartService with cartRepository and userRepository
-		// as arguments for the constructor
-		const cartService = new CartService(
-			cartRepository,
-			userRepository,
-			productsRepository
-		);
+        // New instance of CartService with cartRepository and userRepository
+        // as arguments for the constructor
+        const cartService = new CartService(
+            cartRepository,
+            userRepository,
+            productsRepository
+        );
 
-		// Wait for result of service to get cart
-		const result: Record<string, any> = await cartService.getCart(headers);
+        // Wait for result of service to get cart
+        const result: Record<string, any> = await cartService.getCart(headers);
 
-		// Return both status code and response
-		return res.status(result.code).json(result.response);
-	} catch (error) {
-		// Error handling
-		console.error(error);
-		return res.status(500).json({
-			data: null,
-			error: { message: "Internal Server error" },
-		});
-	}
+        // Return both status code and response
+        return res.status(result.code).json(result.response);
+    } catch (error) {
+        // Error handling
+        console.error(error);
+        return res.status(500).json({
+            data: null,
+            error: { message: "Internal Server error" },
+        });
+    }
 }
 
 export async function updateCart(req: Request, res: Response) {
-	try {
-		// Get request headers
-		const headers: Record<string, any> = req.headers;
+    try {
+        // Get request headers
+        const headers: Record<string, any> = req.headers;
 
-		// Get request body
-		const body: Record<string, any> = req.body;
+        // Get request body
+        const body: Record<string, any> = req.body;
 
-		// New instance of CartRepository
-		const cartRepository = new CartRepository();
+        // New instance of CartRepository
+        const cartRepository = new CartRepository();
 
-		// New instance of UserRepository
-		const userRepository = new UserRepository();
+        // New instance of UserRepository
+        const userRepository = new UserRepository();
 
-		// New instance of ProductsRepository
-		const productsRepository = new ProductsRepository();
+        // New instance of ProductsRepository
+        const productsRepository = new ProductsRepository();
 
-		// New instance of CartService with cartRepository and userRepository
-		// as arguments for the constructor
-		const cartService = new CartService(
-			cartRepository,
-			userRepository,
-			productsRepository
-		);
+        // New instance of CartService with cartRepository and userRepository
+        // as arguments for the constructor
+        const cartService = new CartService(
+            cartRepository,
+            userRepository,
+            productsRepository
+        );
 
-		// Wait for result of service to get cart
-		const result: Record<string, any> = await cartService.updateCart(
-			headers,
-			body
-		);
+        // Wait for result of service to get cart
+        const result: Record<string, any> = await cartService.updateCart(
+            headers,
+            body
+        );
 
-		// Return both status code and response
-		return res.status(result.code).json(result.response);
-	} catch (error) {
-		// Error handling
-		console.error(error);
-		return res.status(500).json({
-			data: null,
-			error: { message: "Internal Server error" },
-		});
-	}
+        // Return both status code and response
+        return res.status(result.code).json(result.response);
+    } catch (error) {
+        // Error handling
+        console.error(error);
+        return res.status(500).json({
+            data: null,
+            error: { message: "Internal Server error" },
+        });
+    }
 }
 
 export async function emptyCart(req: Request, res: Response) {
-	try {
-		// Get request headers
-		const headers: Record<string, any> = req.headers;
+    try {
+        // Get request headers
+        const headers: Record<string, any> = req.headers;
 
-		// New instance of CartRepository
-		const cartRepository = new CartRepository();
+        // New instance of CartRepository
+        const cartRepository = new CartRepository();
 
-		// New instance of UserRepository
-		const userRepository = new UserRepository();
+        // New instance of UserRepository
+        const userRepository = new UserRepository();
 
-		// New instance of ProductsRepository
-		const productsRepository = new ProductsRepository();
+        // New instance of ProductsRepository
+        const productsRepository = new ProductsRepository();
 
-		// New instance of CartService with cartRepository and userRepository
-		// as arguments for the constructor
-		const cartService = new CartService(
-			cartRepository,
-			userRepository,
-			productsRepository
-		);
+        // New instance of CartService with cartRepository and userRepository
+        // as arguments for the constructor
+        const cartService = new CartService(
+            cartRepository,
+            userRepository,
+            productsRepository
+        );
 
-		// Wait for result of service to empty cart
-		const result: Record<string, any> = await cartService.emptyCart(
-			headers
-		);
+        // Wait for result of service to empty cart
+        const result: Record<string, any> = await cartService.emptyCart(
+            headers
+        );
 
-		// Return both status code and response
-		return res.status(result.code).json(result.response);
-	} catch (error) {
-		// Error handling
-		console.error(error);
-		return res.status(500).json({
-			data: null,
-			error: { message: "Internal Server error" },
-		});
-	}
+        // Return both status code and response
+        return res.status(result.code).json(result.response);
+    } catch (error) {
+        // Error handling
+        console.error(error);
+        return res.status(500).json({
+            data: null,
+            error: { message: "Internal Server error" },
+        });
+    }
 }
